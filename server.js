f2211fbbe24fd84a68f0daffb71d85202c080c65
Omar Portillo <OmrPrt@aol.com>
2018-04-18 17:12:20 +0100

    setting up node server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const PORT = process.env.port || 4000;
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
+
+app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`));
