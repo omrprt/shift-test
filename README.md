d5b771c9d256f9de7ff89664bdd8db19d2ad0ba7
Omar Portillo <OmrPrt@aol.com>
2018-04-18 14:33:56 +0100

    have a render format, have 1000 shift created, have random hours created, have random assignment of unfullied to other.

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-test 
+Messly Front End Technical Assessment
+
+  1. Please do not spend more than 3 or so hours on this.
+  2. Please don't try and make it look "pretty" with colours and fonts. We only care about the desktop & mobile layouts.
+  3. Feel free to use any libraries or JS tools you think necessary.
+
+Task List
+
+  1. Build a function `makeDummyShift()` that generates a random "shift" object which has the following fields:
+
+    a. a duration in hours (which will be a random integer from 1-12).
+    b. a status property (initially UNFILLED) which can change to: CONFIRMED or CANCELLED.
+
+  2. Build a function `getNewStatus( shift )` that returns a new random status according to the following transition probabilities:
+
+    a. UNFILLED => UNFILLED where p = 0.97
+    b. UNFILLED => CONFIRMED where p = shift.duration > 6 ? 0.02 : 0.01
+    c. UNFILLED => CANCELLED where p = shift.duration > 6 ? 0.01 : 0.02
+
+  3. Using Redux, construct some front-end state that holds 1000 shifts generated via the `makeDummyShift` function. The shifts should tick/update every 1 second using the `getNewStatus` function
+
+  4. Construct a pie chart widget showing proportions of shifts in various statuses (again just to be clear - please feel free to use a charting library!).
+
+  5. Construct a bar chart widget showing proportion of shifts in various statuses grouped into sensible "duration buckets" (i.e. 0-4 hours, 4-8 hours, 8-12 hours).
+
+  6. Construct 4 widgets each containing a summary value:
+
+    a. Total number of unfilled shifts
+    b. Total number of confirmed shifts
+    c. Total number of cancelled shifts
+    d. Total number of confirmed or unfilled shifts.
+
+  7. Please layout your widgets using a grid system similar to "Bootstrap Grid" such that:
+
+    a. For desktop screens:
+
+      [ summary 1 ] [ summary 2 ] [ summary 3 ] [ summary 4 ]
+      [ bar chart               ] [ pie chart               ]
+
+    b. For mobile screens:
+
+      [ summary 1 ] [ summary 2 ]
+      [ summary 3 ] [ summary 4 ]
+      [ bar chart               ]
+      [ pie chart               ]
--- src/app.js
+++ src/app.js
@@ -1,21 +1,94 @@
-// const React = require('react');
-// React is a class, and first letter is capitalize.
-// the from in green comes from what they are called in package.json
-
 import React    from 'react';
 import ReactDom from 'react-dom';
 
-//this is call classical companent
+import 'bootstrap-css-only';
+import './scss/style.scss';
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shifts: []
+    };
+    this.makeDummyShift = this.makeDummyShift.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.seconds = 999;
+    this.timer = 0;
+  }
+
+  makeDummyShift() {
+
+    const seconds = this.seconds --;
+    const newHours = Math.floor(Math.random() * 12 + 1);
+    let status = 'UNFILLED';
+
+    const p = (Math.random() + .01).toFixed(2);
+    console.log(newHours, p);
+
+    if ((newHours > 6 && p <= .01) || (newHours <= 6 && p > .01 && p <= .03 )) {
+      status = 'CANCELLED';
+    } else if ((newHours > 6 && p <= .03) || (newHours <= 6 && p <= .01)) {
+      status = 'CONFIRMED';
+    } else {
+      status = 'UNFILLED';
+    }
+    if (seconds === 0) {
+      clearInterval(this.timer);
+    }
+
+    const shiftArray = this.state.shifts;
+
+    shiftArray.push({shift: {
+      hours: newHours,
+      status: status
+    }});
+
+    this.setState({
+      shifts: shiftArray
+    }, () => console.log(this.state.shifts));
+
+  }
+
+  startTimer() {
+    if (this.timer === 0) {
+      this.timer = setInterval(this.makeDummyShift, 1000
+      );
+    }
+  }
+
   render(){
-    // return React.createElement('div', {},
-    //   React.createElement('hi', {}, 'I Like React'),
-    //   React.createElement('h2', {}, 'I really do')
-    // );
+
     return (
-      <div>
-        <h1>I love JSX!</h1>
-        <h2>I also love React more... because of JSX</h2>
+      <div className="container">
+        <h1>Messly Shifts</h1>
+        <div className="Row">
+          <div className="col-md-3 col-xs-6">
+            <p>unfilled shifts</p>
+          </div>
+          <div className="col-md-3 col-xs-6">
+            <p> confirmed shifts</p>
+          </div>
+          <div className="col-md-3 col-xs-6">
+            <p>cancelled shifts</p>
+          </div>
+          <div className="col-md-3 col-xs-6">
+            <p>confirmed or unfilled shifts</p>
+          </div>
+        </div>
+        <div className="Row">
+          <div className="col-md-6">
+            bar chart
+          </div>
+          <div className="col-md-6">
+            pie chart
+          </div>
+        </div>
+      
+        <div>
+          <button className="button" onClick={this.startTimer}>
+          click
+          </button>
+        </div>
       </div>
     );
   }
